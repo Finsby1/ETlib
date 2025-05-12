68d29b52954ff07fb155c0e97fdb96332a1ded29
Finsby1 <bej002@edu.zealand.dk>
2025-05-12 11:14:04 +0200

    asdwq

---
+++ ETlib/EnergyPrice.cs
@@ -0,0 +1,85 @@
+﻿namespace ETlib;
+
+public class EnergyPrice
+{
+    public int Id { get; set; }
+    
+    public double _dkkPrice;
+
+    public DateTime _time;
+
+    public string _category;
+
+
+    
+    
+    public double DkkPrice
+    {
+        get => _dkkPrice;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Value cannot be negative");
+            }
+
+            if (value >= 20)
+            {
+                throw new ArgumentOutOfRangeException("Value cannot be more than 19");
+            }
+            _dkkPrice = value;
+        }
+    }
+
+    public DateTime Time
+    {
+        get => _time;
+        set
+        {
+            if (value >= new DateTime(2125, 1, 1, 0, 0, 0))
+            {
+                throw new ArgumentOutOfRangeException("Value must be less than 2125");
+            }
+
+            if (value <= new DateTime(2025, 1, 1, 0, 0, 0))
+            {
+                throw new ArgumentOutOfRangeException("Value must be greater than 2025");
+            }
+            _time = value;
+        }
+    }
+
+    public string Category
+    {
+        get => _category;
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Category cannot be null");
+            }
+
+            if (value.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Category cannot be less than 3 characters");
+            }
+            _category = value;
+        }
+    }
+
+    public EnergyPrice(double dkkPrice, DateTime time, string category)
+    {
+        DkkPrice = dkkPrice;
+        Time = time;
+        Category = category;
+    }
+
+    public EnergyPrice()
+    {
+        
+    }
+
+
+
+
+}
--- EnergiTommelTests/UnitTest1.cs
+++ EnergiTommelTests/UnitTest1.cs
@@ -5,26 +5,39 @@ namespace EnergiTommelTests;
 [TestClass]
 public class UnitTest1
 {
-    private Elpris elpris;
+    private EnergyPrice _energyPrice;
     
-    [TestMethod]
 
     [TestInitialize]    
-    
-    public void TestInitialize1()
+    public void TestInitialize()
     {
-        elpris = new Elpris(2.0, new DateTime(2025, 5, 10, 12, 00, 00));
-        // Code to run before each test
+        _energyPrice = new EnergyPrice(2, new DateTime(2025, 5, 12, 12, 00, 00), "high");
     } 
+
+    [TestMethod]
+    public void PriceTest()
+    {
+        Assert.AreEqual(2, _energyPrice.DkkPrice);
+        
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _energyPrice.DkkPrice = 0);
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _energyPrice.DkkPrice = 20);
+    }
     
     [TestMethod]
-    public void ElprisTest1()
+    public void DateTest()
     {
-        Assert.AreEqual(2.0, elpris.Pris);
-        Assert.IsNotNull(elpris.Tid);
-        elpris.Pris = 3.5;
-        Assert.AreEqual(3.5, elpris.Pris);
-        elpris.Tid = new DateTime(2025, 5, 10, 13, 00, 00);
-        Assert.AreEqual(new DateTime(2025, 5, 10, 13, 00, 00), elpris.Tid);
+        Assert.AreEqual(new DateTime(2025, 5, 12, 12, 00, 00), _energyPrice.Time);
+        
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _energyPrice.Time = new DateTime(2025, 1, 1, 0, 00, 00) );
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _energyPrice.Time = new DateTime(2125, 1, 1, 00, 00, 00));
+    }
+    
+    [TestMethod]
+    public void CategoryTest()
+    {
+        
+        Assert.AreEqual("high", _energyPrice.Category);
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => _energyPrice.Category = "ab");
+        Assert.ThrowsException<ArgumentNullException>(() => _energyPrice.Category = null);
     }
 }
